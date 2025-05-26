30cbf011a6fed93b2b09db82ee274789a2dddb33
priyaraghunath <priyankabraghunath98@gmail.com>
2025-05-26 18:23:48 -0400

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -260,7 +260,7 @@ terraform {
     key            = "state/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "Terraform-Logstore"
+    dynamodb_table = "Terraform-Logstor"
   }
 }
 
