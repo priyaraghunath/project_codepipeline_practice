3c57b82ca4f24e4626aaae0386c997b668817263
priyaraghunath <priyankabraghunath98@gmail.com>
2025-05-27 01:19:55 -0400

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,14 +1,28 @@
-terraform {
-  required_version = ">= 1.3.0"
+// provider.tf
 
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "bucketforlogstrore-bootcampproject"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "Terraform-Logstor"
+    encrypt        = true
+  }
 }
 
+# Default provider (fallback to region1)
+provider "aws" {
+  region = var.region1
+}
+
+# Aliased providers
 provider "aws" {
   alias  = "region1"
   region = var.region1
